cc7dcd68fd3b55ee317d277940cab15c04cd6dde
David Chawei Hsu <chawei.hsu@gmail.com>
2011-07-03 12:08:29 -0700

    change to localhost

--- webroot/config.js
+++ webroot/config.js
@@ -1,4 +1,5 @@
 //var REMOTE_IP = "75.6.229.35";
 //var REMOTE_IP = "97.107.142.213"; // chawei's server
-var REMOTE_IP = "174.129.17.131"; // dotcloud's server
+//var REMOTE_IP = "174.129.17.131"; // dotcloud's server
+var REMOTE_IP = "localhost"; // dotcloud's server
 var PORT = "8080";
